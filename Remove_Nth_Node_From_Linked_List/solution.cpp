10d4a64df26b3426bc453166ae69bc8bac3afe61
Prem Dogra <officialprem707@gmail.com>
2026-04-13 23:54:53 +0530

    Rename method to RemoveNthFromEnd

--- Remove_Nth_Node_From_Linked_List/solution.cpp
+++ Remove_Nth_Node_From_Linked_List/solution.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    ListNode* removeNthFromEnd(ListNode* head, int n) {
+    ListNode* RemoveNthFromEnd(ListNode* head, int n) {
         ListNode* dummy = new ListNode(0);
         dummy->next = head;
 
